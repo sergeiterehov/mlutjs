06e244f22b77e3e27d4cdfb67bc34dc059950288
sterehov <sterehov@localhost>
2018-06-13 18:03:07 +0300

    fix

--- pga.c
+++ pga.c
@@ -15,7 +15,7 @@ void lut_train(uint64 * node, byte (* func)(byte, int[]), int args[])
 
     for (byte inputs = 0b0; inputs <= 0b1111; inputs++)
     {
-        *node |= ((func(inputs, args) & 0b1111) << (inputs << 2));
+        *node |= (((uint64) func(inputs, args) & 0b1111) << (inputs << 2));
     }
 }
 
@@ -79,7 +79,7 @@ struct pga_emulator
     int height;
 
     int * stack;
-    int stack_size;
+    int stack_pointer;
 };
 
 struct pga_emulator * pga_emulator_create(struct pga_table * table)
@@ -103,13 +103,108 @@ struct pga_emulator * pga_emulator_create(struct pga_table * table)
         emulator->states[x] = &states_array[x * height];
     }
 
-    emulator->stack_size = 1024;
+    emulator->stack_pointer = 0;
 
-    int * stack = malloc(emulator->stack_size * sizeof(int));
+    int * stack = malloc(1024 * sizeof(int));
 
     emulator->stack = stack;
 
     return emulator;
+}
+
+void debug_number_64(uint64 number)
+{
+    for (int i = 0; i < sizeof(number) * 8; i++)
+    {
+        printf("%i", (number >> i) & 0b1);
+
+        if (i % 4 == 3)
+        {
+            printf(" ");
+        }
+    }
+}
+
+int pga_emulator_render(struct pga_emulator * emulator)
+{
+    int iterations = 0;
+
+    int x, y;
+
+    uint64 ** matrix = emulator->table->matrix;
+
+    int * stack = emulator->stack;
+    int sp = emulator->stack_pointer;
+
+    byte ** states = emulator->states;
+
+    int width = emulator->table->width;
+    int height = emulator->table->height;
+
+    uint64 lut;
+
+    byte inputs, state_new;
+    byte * state_current;
+
+    while (sp > 0)
+    {
+        x = stack[--sp];
+        y = stack[--sp];
+
+        if (x < 0 || y < 0 || x >= width || y >= height)
+        {
+            continue;
+        }
+
+        lut = matrix[x][y];
+
+        x++;
+        y++;
+
+        state_current = &states[x][y];
+
+        inputs = (
+            0b0
+            | (((states[x][y - 1] >> NODE_BOTTOM) & 0b1) << NODE_UP)
+            | (((states[x + 1][y] >> NODE_LEFT) & 0b1) << NODE_RIGHT)
+            | (((states[x][y + 1] >> NODE_UP) & 0b1) << NODE_BOTTOM)
+            | (((states[x - 1][y] >> NODE_RIGHT) & 0b1) << NODE_LEFT)
+        );
+
+        state_new = (lut >> (inputs << 2)) & 0b1111;
+
+        printf("%i: \t", inputs);
+        debug_number_64(lut);
+        printf("\n");
+
+        if (*state_current == state_new)
+        {
+            continue;
+        }
+
+        *state_current = state_new;
+
+        x--;
+        y--;
+
+        iterations++;
+
+        stack[sp++] = x;
+        stack[sp++] = y - 1;
+
+        stack[sp++] = x + 1;
+        stack[sp++] = y;
+
+        stack[sp++] = x;
+        stack[sp++] = y + 1;
+
+        stack[sp++] = x - 1;
+        stack[sp++] = y;
+    }
+
+    emulator->stack_pointer = sp;
+
+    return iterations;
 }
 
 int main()
@@ -119,9 +214,21 @@ int main()
 
     lut_train(&table->matrix[0][1], lut_wire_function, (int[2]) {NODE_UP, NODE_BOTTOM});
     lut_train(&table->matrix[1][0], lut_wire_function, (int[2]) {NODE_LEFT, NODE_RIGHT});
-    lut_train(&table->matrix[1][1], lut_xor_function, (int[3]) {NODE_UP, NODE_LEFT, NODE_RIGHT});
+    lut_train(&table->matrix[1][1], lut_and_function, (int[3]) {NODE_UP, NODE_LEFT, NODE_RIGHT});
 
-    printf("%i\n", table->matrix[1][1]);
+    emulator->states[0][2] = 0b1111;
+    emulator->states[2][0] = 0b1111;
+
+    emulator->stack[emulator->stack_pointer++] = 0;
+    emulator->stack[emulator->stack_pointer++] = 1;
+
+    emulator->stack[emulator->stack_pointer++] = 1;
+    emulator->stack[emulator->stack_pointer++] = 0;
+
+    int iterations = pga_emulator_render(emulator);
+
+    printf("Iterations: %i\n", iterations);
+    printf("State: %i\n", emulator->states[2][2]);
 
     return 0;
 }
