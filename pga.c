96d81c9f41356e057a2b790d196d4c82eb4afd97
sterehov <sterehov@localhost>
2018-06-13 15:11:25 +0300

    C - переработка

--- pga.c
+++ pga.c
@@ -1,156 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct point
+typedef __uint64_t uint64;
+typedef unsigned char byte;
+
+#define NODE_UP 0
+#define NODE_RIGHT 1
+#define NODE_BOTTOM 2
+#define NODE_LEFT 3
+
+void lut_train(uint64 * node, byte (* func)(byte, int[]), int args[])
 {
-    int x;
-    int y;
-};
+    byte outputs = 0b0;
+
+    for (byte inputs = 0b0; inputs <= 0b1111; inputs++)
+    {
+        *node |= ((func(inputs, args) & 0b1111) << (inputs << 2));
+    }
+}
+
+byte lut_wire_function(byte inputs, int args[])
+{
+    return ((inputs >> args[0]) & 0b1) << args[1];
+}
+
+byte lut_not_function(byte inputs, int args[])
+{
+    return (~(inputs >> args[0]) & 0b1) << args[1];
+}
+
+byte lut_and_function(byte inputs, int args[])
+{
+    return (((inputs >> args[0]) & 0b1) & ((inputs >> args[1]) & 0b1)) << args[2];
+}
+
+byte lut_or_function(byte inputs, int args[])
+{
+    return (((inputs >> args[0]) & 0b1) | ((inputs >> args[1]) & 0b1)) << args[2];
+}
+
+byte lut_xor_function(byte inputs, int args[])
+{
+    return (((inputs >> args[0]) & 0b1) ^ ((inputs >> args[1]) & 0b1)) << args[2];
+}
 
 struct pga_table
 {
-    __uint64_t * matrix;
-
+    uint64 ** matrix;
     int width;
     int height;
 };
 
-struct pga_emulator
-{
-    unsigned char * states;
-    struct pga_table * table;
-
-    struct point * stack;
-    int stack_position;
-};
-
 struct pga_table * pga_table_create(int width, int height)
 {
-    struct pga_table * table = aligned_alloc(1, sizeof(struct pga_table));
+    struct pga_table * table = malloc(1 * sizeof(struct pga_table));
 
     table->width = width;
     table->height = height;
-    table->matrix = aligned_alloc(width * height, sizeof(__uint64_t));
+
+    table->matrix = malloc(width * sizeof(uint64 *));
+
+    uint64 * matrix_array = malloc(width * height * sizeof(uint64));
+
+    for (int x = 0; x < width; x++)
+    {
+        table->matrix[x] = &matrix_array[x * height];
+    }
 
     return table;
 }
 
+struct pga_emulator
+{
+    struct pga_table * table;
+
+    byte ** states;
+    int width;
+    int height;
+
+    int * stack;
+    int stack_size;
+};
+
 struct pga_emulator * pga_emulator_create(struct pga_table * table)
 {
-    struct pga_emulator * emulator = aligned_alloc(1, sizeof(struct pga_emulator));
+    struct pga_emulator * emulator = malloc(sizeof(struct pga_emulator));
+
+    int width = table->width + 2;
+    int height = table->height + 2;
 
     emulator->table = table;
-    emulator->states = aligned_alloc((table->width + 2) * (table->height + 2), sizeof(char));
-    emulator->stack = aligned_alloc(100, sizeof(struct point));
-    emulator->stack_position = 0;
+
+    emulator->width = width;
+    emulator->height = height;
+
+    emulator->states = malloc(width * sizeof(byte *));
+
+    byte * states_array = malloc(width * height * sizeof(byte));
+
+    for (int x = 0; x < width; x++)
+    {
+        emulator->states[x] = &states_array[x * height];
+    }
+
+    emulator->stack_size = 1024;
+
+    int * stack = malloc(emulator->stack_size * sizeof(int));
+
+    emulator->stack = stack;
 
     return emulator;
-}
-
-void pga_emulator_stack_push(struct pga_emulator * emulator, int x, int y)
-{
-    struct point * record = &(emulator->stack[emulator->stack_position++]);
-
-    record->x = x;
-    record->y = y;
-}
-
-struct point * pga_emulator_stack_pop(struct pga_emulator * emulator)
-{
-    return &(emulator->stack[--emulator->stack_position]);
-}
-
-int pga_emulator_propagate(struct pga_emulator * emulator)
-{
-    int interations = 0;
-
-    struct point * now;
-
-    int width = emulator->table->width;
-    int height = emulator->table->height;
-
-    __uint64_t * table = emulator->table->matrix;
-
-    int x;
-    int y;
-
-    __uint64_t lut;
-
-    unsigned char inputs;
-    unsigned char state;
-    unsigned char * stateCurrent;
-
-    while (emulator->stack_position > 0)
-    {
-        now = pga_emulator_stack_pop(emulator);
-
-        x = now->x;
-        y = now->y;
-
-        if (x == -1 || y == -1 || x == width || y == height)
-        {
-            continue;
-        }
-
-        lut = table[x, y];
-
-        x++;
-        y++;
-
-        inputs = (
-            0b0
-            | ( ((emulator->states[x, y - 1] >> 2) & 0b1) << 0 )
-            | ( ((emulator->states[x + 1, y] >> 3) & 0b1) << 1 )
-            | ( ((emulator->states[x, y + 1] >> 0) & 0b1) << 2 )
-            | ( ((emulator->states[x - 1, y] >> 1) & 0b1) << 3 )
-        );
-
-        state = ( (lut >> inputs) & 0b1111 );
-        stateCurrent = &(emulator->states[x, y]);
-
-        if (state == *stateCurrent)
-        {
-            continue;
-        }
-
-        *stateCurrent = state;
-
-        x--;
-        y--;
-
-        pga_emulator_stack_push(emulator, x, y - 1);
-        pga_emulator_stack_push(emulator, x + 1, y);
-        pga_emulator_stack_push(emulator, x, y + 1);
-        pga_emulator_stack_push(emulator, x - 1, y);
-
-        interations++;
-    }
-
-    return interations;
-}
-
-__uint64_t lut_train(unsigned char (*f)(unsigned char))
-{
-    __uint64_t lut = 0b0;
-
-    for (unsigned char inputs = 0b0; inputs <= 0b1111; inputs++)
-    {
-        lut |= f(inputs) << (inputs * 4);
-    }
-
-    return lut;
-}
-
-__uint64_t lut_and(int a, int b, int f)
-{
-    __uint64_t result = 0b0;
-
-    for (int i = 0b0; i <= 0b1111; i++)
-    {
-        result |= (((i >> a) & 0b1) & ((i >> b) & 0b1)) << f;
-    }
-
-    return result;
 }
 
 int main()
@@ -158,13 +117,11 @@ int main()
     struct pga_table * table = pga_table_create(3, 3);
     struct pga_emulator * emulator = pga_emulator_create(table);
 
-    table->matrix[0, 0] = lut_and(0, 3, 1);
+    lut_train(&table->matrix[0][1], lut_wire_function, (int[2]) {NODE_UP, NODE_BOTTOM});
+    lut_train(&table->matrix[1][0], lut_wire_function, (int[2]) {NODE_LEFT, NODE_RIGHT});
+    lut_train(&table->matrix[1][1], lut_xor_function, (int[3]) {NODE_UP, NODE_LEFT, NODE_RIGHT});
 
-    pga_emulator_stack_push(emulator, 0, 0);
-
-    printf("LUT: %i\n", table->matrix[0, 0]);
-    // todo: Происходит какая то херня. По пьяне не понятно
-    printf("Iterations: %i\n", pga_emulator_propagate(emulator));
+    printf("%i\n", table->matrix[1][1]);
 
     return 0;
 }
